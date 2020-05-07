a5e92a8288d9114b6d7a99a4395e431c7de0d57c
Devesh Anand <ananddeves22@gmail.com>
2020-05-07 11:29:37 +0530

    declaration file added

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,9 @@
-declare namespace policies {
-    interface policies {
-        SUPER: string,
-        ADMIN_VIEW: string,
-        ADMIN_PUBLISH: string,
-        EMPLOYEE_VIEW: string,
-        EMPLOYEE_PUBLISH: string,
-        VIEW_CATALOGUE: string,
-        PUBLISH_CATALOGUE: string
-    }
+declare namespace index {
+    SUPER: string
+    ADMIN_VIEW: string
+    ADMIN_PUBLISH: string
+    EMPLOYEE_VIEW: string
+    EMPLOYEE_PUBLISH: string
+    VIEW_CATALOGUE: string
+    PUBLISH_CATALOGUE: string
 }
