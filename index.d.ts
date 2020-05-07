60cf4c589326a592d1f636547738c8493c21eb5d
Devesh Anand <ananddeves22@gmail.com>
2020-05-07 11:53:08 +0530

    changes

--- index.d.ts
+++ index.d.ts
@@ -3,5 +3,5 @@ export const ADMIN_VIEW : string
 export const ADMIN_PUBLISH : string
 export const EMPLOYEE_VIEW : string
 export const EMPLOYEE_PUBLISH : string
-export const VIEW_CATALOGUE : string
-export const PUBLISH_CATALOGUE : string
+export const CATALOGUE_VIEW : string
+export const CATALOGUE_PUBLISH : string
