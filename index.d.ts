8e790c311efc12fcc6db6dd9029357bf79eb2deb
Devesh Anand <ananddeves22@gmail.com>
2020-05-07 11:35:37 +0530

    declaration file added

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,7 @@
-declare namespace policies {
-    SUPER: string
-    ADMIN_VIEW: string
-    ADMIN_PUBLISH: string
-    EMPLOYEE_VIEW: string
-    EMPLOYEE_PUBLISH: string
-    VIEW_CATALOGUE: string
-    PUBLISH_CATALOGUE: string
-}
+export const SUPER : string
+export const ADMIN_VIEW : string
+export const ADMIN_PUBLISH : string
+export const EMPLOYEE_VIEW : string
+export const EMPLOYEE_PUBLISH : string
+export const VIEW_CATALOGUE : string
+export const PUBLISH_CATALOGUE : string
